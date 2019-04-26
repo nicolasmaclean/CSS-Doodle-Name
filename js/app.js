5148baa73b726422c875c3f718712ebc6856e8f2
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-04-26 13:19:15 -0500

    set up

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// import 'css-doodle';
+
+const doodle = document.querySelector('css-doodle');
+const Ndoodle = document.querySelector('N');
+
+window.addEventListener('resize', init);
+
+function init() {
+    
+}
+
+init();
